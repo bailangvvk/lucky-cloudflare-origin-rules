22b389486db3e39960423eac456a4620af5cd204
bailangvvk <94160310+bailangvvk@users.noreply.github.com>
2024-10-05 17:04:20 +0800

    Update origin-rules.sh

--- origin-rules.sh
+++ origin-rules.sh
@@ -3,37 +3,9 @@
 # 设置必要的变量
 email="你的cloudflare邮箱"
 api_key="你的cloudflare GLOBAL API KEY"
-domain="lovelyy.us.kg"  # 主域名信息
-rule_description="Openwrt Nginx cdn4 > port"  # 需要更新描述的规则
+domain="xxxx.us.kg"  # 主域名信息
+rule_description="Openwrt Nginx cdn4 > port"  # 需要更新描述的规则 这个必须要origin-rules有的 不然匹配不上
 new_port=${port}  # 你想要设置的新端口默认是lucky传递的参数
-
-# 锁文件路径
-lockfile="/tmp/cloudflare_update.lock"
-
-# 随机睡眠 0 到 1 秒
-sleep_time=$(awk -v min=0 -v max=1000 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
-sleep $(echo "scale=3; $sleep_time / 1000" | bc)
-
-# 等待锁文件，最多等 20 秒
-max_wait=20
-wait_time=0
-
-while [ -f "$lockfile" ]; do
-  # 如果锁文件存在，等待1秒
-  sleep 1
-  wait_time=$((wait_time + 1))
-  
-  if [ "$wait_time" -ge "$max_wait" ]; then
-    echo "等待锁文件释放超时，脚本退出。"
-    exit 1
-  fi
-done
-
-# 创建锁文件
-touch "$lockfile"
-
-# 捕获脚本退出时删除锁文件
-trap 'rm -f "$lockfile"' EXIT
 
 # 获取 zone_id
 zone_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${domain}" \
@@ -62,6 +34,12 @@ rules=$(echo "$ruleset_response" | jq -r '.result.rules')
 # 找到需要更新的规则 ID
 rule_id=$(echo "$rules" | jq -r --arg description "$rule_description" '.[] | select(.description == $description) | .id')
 
+# 检查是否找到 rule_id
+# if [ -z "$rule_id" ]; then
+#   echo "未找到描述为 '${rule_description}' 的规则。"
+#   exit 1
+# fi
+
 # 创建更新后的规则列表
 updated_rules=$(echo "$rules" | jq --arg id "$rule_id" --argjson port "$new_port" '
   map(
@@ -83,12 +61,13 @@ update_response=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${z
   }")
 
 # 检查更新响应
-# if echo "$update_response" | jq -e '.success' > /dev/null; then
-#   echo "规则端口已成功更新为 ${new_port}。"
-# else
-#   echo "更新规则端口时出现错误:"
-#   echo "$update_response" | jq .
-#   exit 1
-# fi
+if echo "$update_response" | jq -e '.success' > /dev/null; then
+  echo "规则端口已成功更新为 ${new_port}。"
+else
+  echo "更新规则端口时出现错误:"
+  echo "$update_response" | jq .
+  exit 1
+fi
+
 
 # 脚本执行完毕，锁文件自动删除
